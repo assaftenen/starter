067c7e5cb14d25e72b975483e4421595eaa1fed7
Assaf <assaf.tenen@gmail.com>
2016-09-04 07:14:36 +0300

    4

--- seeds/js/todo.controller.js
+++ seeds/js/todo.controller.js
@@ -1,18 +1,30 @@
 function TodoController() {
-  this.list = [
-    {
-      title: 'First todo item!',
-      completed: true
-    },
-    {
-      title: 'Second todo item!',
-      completed: false
-    },
-    {
-      title: 'Third todo item!',
-      completed: false
-    }]
-
-};
-angular.module('app')
+	this.newTodo='';
+	this.list = [
+		{
+			title: 'First todo item!',
+			completed: true
+		},
+		{
+			title: 'Second todo item!',
+			completed: false
+		},
+		{
+			title: 'Third todo item!',
+			completed: false
+		}];
+	this.addTodo = function () {
+		this.list.unshift({
+			title: this.newTodo,
+			completed: false
+		})
+	}
+	this.removeTodo = function (item, index) {
+		this.list.splice(index, 1);
+	}
+	
+	
+	
+}
+	angular.module('app')
   .controller('TodoController', TodoController);
